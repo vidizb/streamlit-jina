7eeb300ef9387bd905a1edc8ad19b3f63bf14322
vidizb <107489313+vidizb@users.noreply.github.com>
2023-05-02 20:01:24 +0700

    Update jina_text.py

--- jina_text.py
+++ jina_text.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_jina import jina
 st.set_page_config(page_title="Jina Text Search",)
 
-endpoint = "http://0.0.0.0:45678/api/search"
+endpoint = "https://vidizb-streamlit-jina-jina-text-e5vbu7.streamlit.app/api/search"
 
 st.title("Jina Text Search")
 st.markdown("You can run our [Wikipedia search example](https://github.com/jina-ai/examples/tree/master/wikipedia-sentences) to test out this search")
